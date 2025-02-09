3fa2f2dcc4332d95c64e3c410b62623bad2844b5
Mirzohidde4 <mirzohidxudayberganov27@gmail.com>
2025-02-09 14:38:29 +0500

    view

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path
+from main.views import HomeView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', HomeView, name='home'),
 ]
